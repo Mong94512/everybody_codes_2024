3f7dfe475c2a0db82acd0010c0c8f9cafa2d11c5
Mong <maicanttouchthis@gmail.com>
2026-01-10 09:53:21 +0800

    Added part2 solution by merging it with part1 solution

--- day9/main.py
+++ day9/main.py
@@ -1,20 +1,24 @@
 
-def solve1() -> int:
+def solve_1_2(filename: str) -> int:
     
     targets: list[int] = []
+    stamps: list[int] = []
 
-    with open("input1.in") as fin:
-        targets = list(map(int, fin.readlines()))
+    with open(filename) as fin:
+        while s := fin.readline():
+            if s == "\n":
+                break
+            targets.append(int(s.strip()))
+
+        stamps = [int(s) for s in fin.readline().split(", ")]
 
     MAX_TARGET: int = max(targets)
     INF: int = 1 << 30
-    STAMPS: list[int] = [1, 3, 5, 10]
     dp: list[int] = [INF] * (MAX_TARGET + 1)
-
     dp[0] = 0
 
     for target in range(1, MAX_TARGET + 1):
-        for stamp in STAMPS:
+        for stamp in stamps:
             if target - stamp >= 0:
                 dp[target] = min(dp[target], dp[target - stamp] + 1)
 
@@ -28,5 +32,8 @@ def solve1() -> int:
 
 if __name__ == "__main__":
 
-    ans1: int = solve1()
+    ans1: int = solve_1_2("input1.in")
     print(f"{ans1=}")
+
+    ans2: int = solve_1_2("input2.in")
+    print(f"{ans2=}")
