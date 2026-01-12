8db0bb955895b05b767021c0593dab1a1a0a2e5c
Mong <maicanttouchthis@gmail.com>
2026-01-13 07:05:23 +0800

    Added part3 solution

--- day14/main.py
+++ day14/main.py
@@ -1,3 +1,6 @@
+from typing import TypeAlias
+
+vec3t: TypeAlias = tuple[int, int, int]
 
 dirs: dict[str, list[int]] = {
     "U": [0, 1, 0],
@@ -7,6 +10,7 @@ dirs: dict[str, list[int]] = {
     "F": [0, 0, 1],
     "B": [0, 0, -1]
 }
+
 
 def solve1() -> int:
 
@@ -38,7 +42,7 @@ def solve2() -> int:
         while line := fin.readline():
             queries.append(line.strip().split(","))
 
-    vis: set[tuple[int, int, int]] = set()
+    vis: set[vec3t] = set()
 
     def go_mark(query: list[str]) -> None:
         #(x-hori, y-vert, z-depth)
@@ -53,13 +57,88 @@ def solve2() -> int:
                 for i, mag in enumerate(dirs[dir]):
                     pos[i] += mag
                     
-                vis.add(tuple(pos)) # type: ignore
+                vis.add(vec3t(pos))
 
 
     for query in queries:
         go_mark(query) 
 
     return len(vis)
+
+
+def solve3() -> int:
+
+    INF: int = 1 << 30
+    branches: list[list[str]] = []
+    points: set[vec3t] = set()
+    leaves: set[vec3t] = set()
+    trunks: dict[vec3t, list[int]] = {}
+
+    with open("input3.in") as fin:
+        while line := fin.readline():
+            branches.append(line.strip().split(","))
+
+
+    def add_points(branch: list[str]) -> list[int]:
+        #(x-hori, y-vert, z-depth)
+        pos: list[int] = [0, 0, 0]
+
+        for b in branch:
+            dir: str = b[0]
+            step: int = int(b[1:])
+
+            while step:
+                step -= 1
+                for i, mag in enumerate(dirs[dir]):
+                    pos[i] += mag
+                    
+                points.add(vec3t(pos))
+
+        return pos
+
+
+    def bfs(src: vec3t) -> None:
+        
+        que: list[vec3t] = [src]
+        vis: set[vec3t] = set([src])
+        step = -1
+
+        while que:
+
+            step += 1
+            next_que: list[vec3t] = []
+
+            for cur in que:
+                if cur in trunks:
+                    trunks[cur].append(step)
+
+                for dir in dirs.values():
+                    nei = vec3t(p + d for p, d in zip(cur, dir))
+                    if nei in points and nei not in vis:
+                        next_que.append(nei)
+                        vis.add(nei)
+
+            que = next_que
+
+
+    for branch in branches:
+        leaves.add(vec3t(add_points(branch)))
+
+    for y in range(1, INF):
+        if (0, y, 0) not in points:
+            break
+        trunks[(0, y, 0)] = []
+
+    for leaf in leaves:
+        bfs(leaf)
+
+    best: int = INF
+
+    for steps in trunks.values():
+        assert len(steps) == len(leaves)
+        best = min(best, sum(steps))
+
+    return best
 
 
 if __name__ == "__main__":
@@ -69,3 +148,6 @@ if __name__ == "__main__":
     
     ans2: int = solve2()
     print(f"{ans2=}")
+    
+    ans3: int = solve3()
+    print(f"{ans3=}")
